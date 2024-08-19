695817a0c2864be78f5f578fc48e0251982fda2a
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-08-19 13:40:34 +0530

    new files added

---
+++ src/java/controller/L.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class L extends HttpServlet {
+
+}
